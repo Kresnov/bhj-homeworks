58942840d9edfe1bf55446dcd8df66391fba4f43
Alexander Kresnov <kresnov@gmail.com>
2020-01-20 11:34:22 +0300

    Правки

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,16 +1,22 @@
 const hasTooltip = document.querySelectorAll('.has-tooltip');
 const tooltip = document.querySelector('.tooltip');
 
-function showTooltip(event) {    
+function showTooltip(event) {
     const title = event.target.getAttribute('title');
 
     event.preventDefault();
+    
+    if (tooltip.textContent === title && tooltip.style.display === 'block') {
+        tooltip.style.display = 'none';
+        return
+    } else {
     tooltip.textContent = title;
     tooltip.style.display = 'block';
     tooltip.style.top = `${event.target.getBoundingClientRect().top + 22}px`;
     tooltip.style.left = `${event.target.getBoundingClientRect().left}px`;
+    }
 }
 
 for(let tip of hasTooltip) {
-	tip.addEventListener('click', showTooltip);
+    tip.addEventListener('click', showTooltip);
 }
